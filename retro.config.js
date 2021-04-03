636d84acef56fc2adc939dbf1306b35ef8d4f591
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2021-04-04 04:35:11 +0900

    First commit

---
+++ retro.config.js
@@ -0,0 +1,19 @@
+const sass = {
+	name: "scss",
+	setup(build) {
+		const sass = require("sass")
+
+		build.onLoad({ filter: /\.scss$/ }, args => {
+			const result = sass.renderSync({ file: args.path })
+			return {
+				contents: result.css.toString(),
+				loader: "css",
+			}
+		})
+	},
+}
+
+module.exports = {
+	target: ["es2017"],
+	plugins: [sass],
+}
---
+++ src/App.js
@@ -0,0 +1,143 @@
+import "./App.scss"
+
+function buildStyleObject(dir) {
+	const {
+		durMS: _1, // No-op
+		func: _2,  // No-op
+		x,
+		y,
+		scale,
+		...rest
+	} = dir
+
+	const out = { ...rest }
+	if (x !== undefined || y !== undefined || scale !== undefined) {
+		let transformStr = ""
+		if (x !== undefined) {
+			if (transformStr !== "") transformStr += " "
+			transformStr += `translateX(${typeof x === "string" ? x : `${x}px`})`
+		}
+		if (y !== undefined) {
+			if (transformStr !== "") transformStr += " "
+			transformStr += `translateY(${typeof y === "string" ? y : `${y}px`})`
+		}
+		if (scale !== undefined) {
+			if (transformStr !== "") transformStr += " "
+			transformStr += `scale(${scale})`
+		}
+		transformStr += " "
+		transformStr += `translateZ(0)`
+
+		out.transform = transformStr
+	}
+	return out
+}
+
+function aliases(arr) {
+	const out = arr.map(v => {
+		switch (v) {
+			case "durMS": // No-op
+			case "func":  // No-op
+				return undefined
+			case "x":
+			case "y":
+			case "scale":
+				return "transform"
+		}
+		// Convert to kebab-case
+		return v.replace(/([A-Z])/g, (_, $1, x) => {
+			return (x === 0 ? "" : "-") + $1.toLowerCase()
+		})
+	})
+	return out.filter(v => v !== undefined)
+}
+
+const Transition = React.forwardRef(({
+	on,
+	from,
+	to,
+	durMS,
+	func,
+	children,
+}, ref) => {
+	if (!React.isValidElement(children)) {
+		throw new Error("!React.isValidElement(children)")
+	}
+
+	ref ??= children.ref
+
+	const [computedStyles, setComputedStyles] = React.useState(buildStyleObject(from))
+	const [computedDurMS, setComputedDurMS] = React.useState(from.durMS ?? durMS ?? 300)
+	const [computedFunc, setComputedFunc] = React.useState(from.func ?? func ?? "ease-out")
+
+	React.useEffect(() => {
+		const target = !on ? from : to
+		setComputedStyles(buildStyleObject(target))
+		setComputedDurMS(target.durMS ?? durMS ?? 300)
+		setComputedFunc(target.func ?? func ?? "ease-out")
+	}, [on, from, to, durMS, func])
+
+	const distinct = [
+		...new Set([
+			...aliases(Object.keys(from)),
+			...aliases(Object.keys(to)),
+		]),
+	]
+
+	return React.cloneElement(
+		children,
+		{
+			ref,
+			style: {
+				...computedStyles,
+				willChange: distinct.join(", "),
+				transition: distinct.map(v => `${v} ${computedDurMS}ms ${computedFunc} 0ms`).join(", "),
+			},
+		}
+	)
+})
+
+export default function App() {
+	const [open, setOpen] = React.useState(false)
+
+	// const ref = React.useRef(null)
+
+	return (
+		<>
+			<button onClick={() => setOpen(!open)}>Press me</button>
+			<div className="center min-h-screen">
+				<Transition
+					// ref={ref}
+					on={open}
+					from={{
+						boxShadow: `
+							0 0 1px hsla(0, 0%, 0%, 0.25),
+							0 0 transparent,
+							0 0 transparent
+						`,
+						opacity: 0,
+						y: -20,
+						scale: 0.75,
+						durMS: 1_000,
+					}}
+					to={{
+						boxShadow: `
+							0 0 1px hsla(0, 0%, 0%, 0.25),
+							0 8px 8px hsla(0, 0%, 0%, 0.1),
+							0 2px 8px hsla(0, 0%, 0%, 0.1)
+						`,
+						opacity: 1,
+						y: 0,
+						scale: 1,
+						durMS: 300,
+					}}
+					func="cubic-bezier(0, 0.75, 0.25, 1.1)"
+				>
+					<div className="modal center">
+						<h1>I love you! ❤</h1>
+					</div>
+				</Transition>
+			</div>
+		</>
+	)
+}
